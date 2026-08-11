e8d4d708edc35589627f281d55e8212db43b7da4
Khoa Tran <154944110+KhoaTran3126@users.noreply.github.com>
2026-08-11 12:27:06 -0500

    Update test_model function to include model_name

--- Utils/test_model.py
+++ Utils/test_model.py
@@ -1,4 +1,4 @@
-def test_model(model, X_test, y_test, scorer):
+def test_model(model, model_name, X_test, y_test, scorer):
   y_preds = model.predict_proba(X_test)[:, -1]
   score = scorer(y_test, y_preds)
-  print(f"Model's Score on Test Set: {score:.3f}")
+  print(f"Score of {model_name} on Test Set: {score:.3f}")
