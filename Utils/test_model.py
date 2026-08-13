5452c1fe0b4e9130e1ceb681ae914b66f4af85d8
Khoa Tran <154944110+KhoaTran3126@users.noreply.github.com>
2026-08-13 14:56:48 -0500

    Update test_model.py

--- Utils/test_model.py
+++ Utils/test_model.py
@@ -15,4 +15,4 @@ def test_model(model, model_name, X_test, y_test, scorer=average_precision_score
     """
     y_preds = model.predict_proba(X_test)[:, -1]
     score   = scorer(y_test, y_preds)
-    print(f"PR-AUC of {model_name} on Test Set: {score:.3f}")
+    print(f"Average Precision of {model_name} on Test Set: {score:.3f}")
